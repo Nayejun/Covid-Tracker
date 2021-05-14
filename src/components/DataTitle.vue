d33e276a8397b40e488d2649af1b72bd06ef9d4f
4YJ <67637706+yejun0705@users.noreply.github.com>
2021-05-14 12:24:36 +0900

    Add Data

---
+++ src/components/DataTitle.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="text-center">
+        <h2 class="text-3xl font-bold">{{text}}</h2>
+        <div class="text-2xl mt-4 mb-10">
+            {{dataDate}}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'DataTitle',
+    props: ['text', 'dataDate']
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
-  <div class="home">
-    Hello world
-  </div>
+<!-- 조건부 렌더링 -->
+  <main v-if = "!loding">
+    <DataTitle :text="title" :dataDate="dataDate" />
+  </main>
+
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-gray-500 text-3xl mt-10 mb-6">
+        Fetching Data
+    </div>
+    <img :src="lodingImage" class="w-24 m-auto">
+  </main>
 </template>
 
 <script>
+import DataTitle from '@/components/DataTitle'
+
 export default {
   name: 'Home',
-  components: {},
+  components: {
+    DataTitle
+  },
   data() {
-
+    return {
+      loding: true,
+      title: 'Global',
+      dataDate: '',
+      status: [],
+      countries: [],
+      lodingImage: require('../assets/hourglass.gif')
+    }
   },
   methods: {
-    // 비동기 처리
+    // 비동기 async 사용
     async fetchCovidData() {
       const res = await fetch('https://api.covid19api.com/summary')
       const data = await res.json()
@@ -21,7 +40,15 @@ export default {
   },
   async created() {
     const data = this.fetchCovidData()
-    console.log(data)
+
+    // API에서 들어오는 Date data를 할당
+    this.dataDate = data.Date
+    //글로벌 데이터를 통계에 할당
+    this.stats = data.Global
+    //국가별 데이터
+    this.countries = data.Countries
+    // 데이터 가져올 때 로딩 이미지 표시
+    this.loding = false
   },
 }
 </script>
