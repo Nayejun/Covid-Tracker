a91b77243a3d848abde0805737d05504cdefee08
4YJ <67637706+yejun0705@users.noreply.github.com>
2021-05-17 20:16:45 +0900

    import error

--- src/components/DataBoxes.vue
+++ src/components/DataBoxes.vue
@@ -4,9 +4,22 @@
             <h3 class="text-3xl text-blue-900 font-bold mb-4">Cases</h3>
             <div class="text-2xl mb-4">
                 <span class="font-bold">New:</span>
-                    {{ stats.newComfirmed }}
+                    <!-- {{ stats.NewComfirmed }} -->
+            </div>
+             <div class="text-2xl mb-4">
                 <span class="font-bold">Total:</span>
-                    {{ stats.TotalComfirmed }}
+                    <!-- {{ stats.TotalComfirmed }} -->
+            </div>
+        </div>
+          <div class="shadow-md bg-blue-100 p-10 text-center rounded">
+            <h3 class="text-3xl text-blue-900 font-bold mb-4">Deaths</h3>
+            <div class="text-2xl mb-4">
+                <span class="font-bold">Deaths:</span>
+                    <!-- {{ stats.NewComfirmed }} -->
+            </div>
+             <div class="text-2xl mb-4">
+                <span class="font-bold">Total:</span>
+                    <!-- {{ stats.TotalComfirmed }} -->
             </div>
         </div>
     </div>
